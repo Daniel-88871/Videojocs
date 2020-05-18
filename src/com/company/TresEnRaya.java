f608cc100d1300151af1d64f60d9d0f4c5517b1e
Daniel-88871 <Daniel-88871@github.com>
2020-05-18 16:12:47 +0200

    TresEnRaya

--- src/com/company/TresEnRaya.java
+++ src/com/company/TresEnRaya.java
@@ -37,13 +37,14 @@ public class TresEnRaya {
             System.out.println();
         }
     }
+
     void jugar() {
 
-        for (turno=1; jugar== true ; turno++) {
+        for (turno = 1; jugar == true; turno++) {
             casillaJugador();
-            for (int i = 0; i <3 ; i++) {
+            for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
-                    if (i == x && j==y){
+                    if (i == x && j == y) {
 
                         tablero[i][j] = jugador;
                     }
@@ -59,7 +60,7 @@ public class TresEnRaya {
                 System.out.println();
             }
             comprobarGanador();
-            if (seguirjugando==1){
+            if (seguirjugando == 1) {
                 limpiarpantalla();
                 break;
             }
@@ -67,73 +68,88 @@ public class TresEnRaya {
         }
 
     }
+
     void limpiarpantalla() {
-        for (int i = 0; i <10; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(" ");
 
         }
     }
+
     void comprobarGanador() {
         if ((tablero[0][0] == "X" && tablero[0][1] == "X" && tablero[0][2] == "X" || tablero[0][0] == "X" && tablero[1][0] == "X" && tablero[2][0] == "X" || tablero[0][2] == "X" && tablero[1][2] == "X" && tablero[2][2] == "X" || tablero[2][0] == "X" && tablero[2][1] == "X" && tablero[2][2] == "X" || tablero[1][0] == "X" && tablero[1][1] == "X" && tablero[1][2] == "X" || tablero[0][0] == "X" && tablero[1][1] == "X" && tablero[2][2] == "X" || tablero[0][2] == "X" && tablero[1][1] == "X" && tablero[2][0] == "X" || tablero[0][1] == "X" && tablero[1][1] == "X" && tablero[2][1] == "X")) {
             System.out.println("Gana el jugador X !!");
             System.out.println("Quieres seguir jugando?  ('0' si // '1' no)");
-            seguirjugando=scanner.nextInt();
-            if (seguirjugando==0){
-                turno=0;
-                jugador="X";
-                jugar=true;
+            seguirjugando = scanner.nextInt();
+            if (seguirjugando == 0) {
+                turno = 0;
+                jugador = "X";
+                jugar = true;
                 limpiarTablero();
-            }else if (seguirjugando==1){
-                jugar=false;
+            } else if (seguirjugando == 1) {
+                jugar = false;
                 System.out.println("Gracias por jugar");
                 System.out.println("Saliendo del Tres en raya...");
 
-            }else {
+            } else {
                 System.out.println("Numero no valido");
             }
-        }else if ((tablero[0][0] == "O" && tablero[0][1] == "O" && tablero[0][2] == "O" || tablero[0][0] == "O" && tablero[1][0] == "O" && tablero[2][0] == "O" || tablero[0][2] == "O" && tablero[1][2] == "O" && tablero[2][2] == "O" || tablero[2][0] == "O" && tablero[2][1] == "O" && tablero[2][2] == "O" || tablero[1][0] == "O" && tablero[1][1] == "O" && tablero[1][2] == "O" || tablero[0][0] == "O" && tablero[1][1] == "O" && tablero[2][2] == "O" || tablero[0][2] == "O" && tablero[1][1] == "O" && tablero[2][0] == "O" || tablero[0][1] == "O" && tablero[1][1] == "O" && tablero[2][1] == "O")){
+        } else if ((tablero[0][0] == "O" && tablero[0][1] == "O" && tablero[0][2] == "O" || tablero[0][0] == "O" && tablero[1][0] == "O" && tablero[2][0] == "O" || tablero[0][2] == "O" && tablero[1][2] == "O" && tablero[2][2] == "O" || tablero[2][0] == "O" && tablero[2][1] == "O" && tablero[2][2] == "O" || tablero[1][0] == "O" && tablero[1][1] == "O" && tablero[1][2] == "O" || tablero[0][0] == "O" && tablero[1][1] == "O" && tablero[2][2] == "O" || tablero[0][2] == "O" && tablero[1][1] == "O" && tablero[2][0] == "O" || tablero[0][1] == "O" && tablero[1][1] == "O" && tablero[2][1] == "O")) {
             System.out.println("Gana el jugador O !!");
             System.out.println("Quieres seguir jugando?  ('0' si // '1' no)");
-            seguirjugando=scanner.nextInt();
-            if (seguirjugando==0){
-                turno=0;
-                jugador="X";
-                jugar=true;
+            seguirjugando = scanner.nextInt();
+            if (seguirjugando == 0) {
+                turno = 0;
+                jugador = "X";
+                jugar = true;
                 limpiarTablero();
 
-            }else if (seguirjugando==1){
-                jugar=false;
+            } else if (seguirjugando == 1) {
+                jugar = false;
                 System.out.println("Gracias por jugar");
                 System.out.println("Saliendo del Tres en raya...");
 
-            }else {
+            } else {
                 System.out.println("Numero no valido");
             }
-        }else if (turno==9){
+        } else if (turno == 9) {
             System.out.println("Empate");
             System.out.println("Quieres seguir jugando?  ('0' si // '1' no)");
-            seguirjugando=scanner.nextInt();
-            if (seguirjugando==0){
-                turno=0;
-                jugar=true;
+            seguirjugando = scanner.nextInt();
+            if (seguirjugando == 0) {
+                turno = 0;
+                jugar = true;
                 limpiarTablero();
-            }else if (seguirjugando==1){
-                jugar=false;
+            } else if (seguirjugando == 1) {
+                jugar = false;
                 System.out.println("Gracias por jugar");
                 System.out.println("Saliendo del Tres en raya...");
 
 
-            }else {
+            } else {
                 System.out.println("Numero no valido");
             }
         }
     }
+
     void limpiarTablero() {
-        jugador="X";
-        for (int i = 0; i <3 ; i++) {
+        jugador = "X";
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 tablero[i][j] = " ";
             }
+        }
+    }
+
+    void casillaJugador() {
+        boolean vacia = false;
+        for (; vacia == false; ) {
+            if (turno == 1 || turno == 3 || turno == 5 || turno == 7 || turno == 9) {
+                jugador = "X";
+            } else {
+                jugador = "O";
+            }
+            System.out.println("Escoge tu casilla:");
         }
     }
 }
